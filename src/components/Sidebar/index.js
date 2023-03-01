46e7ef93ddceb3f82cdc58c695f35cbf62ff15b8
MarioGoDevLike <marionassar57@gmail.com>
2023-03-01 17:04:06 +0200

    Finalizing the website 4

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -5,22 +5,45 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faLinkedin, faGithub} from "@fortawesome/free-brands-svg-icons"
 import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
 
-const Sidebar = () => ( 
+const Sidebar = () => {
+    const navbarItems = [{
+        exact : "true",
+        activeclassname : "active",
+        className:"home-link",
+        to:"/",
+        icon:faHome,
+        color:"#4d4d4e"
+    },
+    {
+        exact : "true",
+        activeclassname : "active",
+        className:"about-link",
+        to:"/about",
+        icon:faUser,
+        color:"#4d4d4e"
+    },
+    {
+        exact : "true",
+        activeclassname : "active",
+        className:"contact-link",
+        to:"/contact",
+        icon:faEnvelope,
+        color:"#4d4d4e"
+    }]
+    return (
+    
     <div className="navbar-container">
         <Link className="logo" to="/">
             <img className="myLogo"src={myLogo} alt="logo"/>
         </Link>
         
         <nav className="navbar">
-            <NavLink exact="true" activeclassname="active" className="home-link" to="/">
-                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-                <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
-            </NavLink>
+            {navbarItems.map((item) => {
+                return (<NavLink exact={item.exact} activeclassname={item.activeclassname}
+                className={item.className} to={item.to}>
+                <FontAwesomeIcon icon={item.icon} color={item.color}/>
+            </NavLink>)
+            })}            
         </nav>
 
         <ul className="socials-container">
@@ -37,6 +60,7 @@ const Sidebar = () => (
         </ul>
 
     </div>
-)
+        )
+    }
 
 export default Sidebar;
